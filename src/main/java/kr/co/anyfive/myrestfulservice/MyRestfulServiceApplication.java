42355bb7353e0307db1713d57fbbdd3a9c772013
hjkim <keemye0814@gmail.com>
2024-07-01 14:41:30 +0900

    commit

--- src/main/java/kr/co/anyfive/myrestfulservice/MyRestfulServiceApplication.java
+++ src/main/java/kr/co/anyfive/myrestfulservice/MyRestfulServiceApplication.java
@@ -3,6 +3,11 @@ package kr.co.anyfive.myrestfulservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class MyRestfulServiceApplication {
@@ -11,4 +16,10 @@ public class MyRestfulServiceApplication {
         SpringApplication.run(MyRestfulServiceApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
 }
--- src/main/java/kr/co/anyfive/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/kr/co/anyfive/myrestfulservice/controller/HelloWorldController.java
@@ -1,12 +1,22 @@
 package kr.co.anyfive.myrestfulservice.controller;
 
 import kr.co.anyfive.myrestfulservice.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController //화면단이없음
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
 
     // GET
     // URI - /hello-world
@@ -26,5 +36,10 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloworldInternationalized(
+            @RequestHeader(name="Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
 
 }
